0d6846fb012ea68be8f182b60cc7c1594600a33e
Gituser-PROgrammer <139343533+Gituser-PROgrammer@users.noreply.github.com>
2024-05-20 15:06:44 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js").then(registration => {
-    console.log("service worker has been registered");
-    console.log(registration);
-  }).catch(error => {
-    console.log("service worker has not been registered");
-    console.log(error);
-  });
-}
